dbb902006f0c74c0ee77e8526085deb31d880087
jaspervermeulen <jaspervermeulen.be@icloud.com>
2021-01-22 22:34:43 +0100

    Looked into how to make difference between user and volunteer

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import app from "./base";
-
+import { AuthContext } from "./Auth.js";
 
 const Home = () => {
+
+  const { currentUser } = useContext(AuthContext)
+
   return (
     <>
-      <p>Home page</p>
-      <Link to="scan">Scan</Link>
+      <p>Ben je gebruiker of vrijwilliger?</p>
+      {/* <p>Home page</p>
+      <Link to="scan">Scan</Link>*/}
+      <p>{currentUser.uid}</p> 
       <button onClick={() => app.auth().signOut()}>Sign out</button>  
     </>
   )
