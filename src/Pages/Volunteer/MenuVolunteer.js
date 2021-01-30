bc32f802afc848bbc04e862b52cddaeace62e8f7
jaspervermeulen <jaspervermeulen.be@icloud.com>
2021-01-30 11:55:37 +0100

    added functionality to menu of volunteers

--- src/Pages/Volunteer/MenuVolunteer.js
+++ src/Pages/Volunteer/MenuVolunteer.js
@@ -1,43 +1,19 @@
-import React, { useContext, useState } from "react";
-import { Redirect } from "react-router-dom";
+import React, { useContext } from "react";
+
 import { AuthContext } from "../../Authentication/Auth";
-import firebase from "../../Authentication/base";
 
 const MenuVolunteer = () => {
 
   const { currentUser } = useContext(AuthContext);
-  const [currentUserNaam, setCurrentUserNaam] = useState(undefined)
-
-  const [linkedUsers, setLinkedUsers] = useState([])
-
-  const userRef = firebase.firestore().collection('Users').doc(currentUser.uid);
-  userRef.get().then(function (doc) {
-    if (doc.exists) {
-      if (currentUserNaam === undefined) {
-        setCurrentUserNaam(doc.data().volledige_naam)
-      }
-      setLinkedUsers(doc.data().links);
-    } else {
-      <Redirect to={"/menuvolunteer"} />
-    }
-  })
+  
 
   return (
     <div>
       <p>Volunteer:</p>
-      <p>{currentUserNaam}</p>
+      <p>{currentUser.uid}</p>
       <p>Your users are :</p>
 
-      {
-        linkedUsers.map(linkedUser => {
-          return (
-            <div>
-              <p>{linkedUser.Naam}</p>
-              <p>{linkedUser.Bio}</p>
-            </div>
-          )
-        })
-      }
+      
 
     </div>
   )
